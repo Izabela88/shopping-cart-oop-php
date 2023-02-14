755d23dba4ada1c696f6caeac3fc5bc61a845090
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-02-14 11:45:37 +0000

    ADD: generate construct method in CartItem class

--- CartItem.php
+++ CartItem.php
@@ -8,6 +8,11 @@ class CartItem
 
     // TODO Generate constructor with all properties of the class
     // TODO Generate getters and setters of properties
+    public function __construct(Product $product, int $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
 
     public function increaseQuantity()
     {
